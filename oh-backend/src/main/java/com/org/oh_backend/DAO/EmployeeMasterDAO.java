ea30804d4591bd296fb9a414c7d4f373bfc0d292
Archit Kashyap <architkashyap13@gmail.com>
2018-05-05 17:29:00 +0530

    rectified dao methods

---
+++ oh-backend/src/main/java/com/org/oh_backend/DAO/EmployeeMasterDAO.java
@@ -0,0 +1,18 @@
+package com.org.oh_backend.DAO;
+
+import java.util.List;
+
+import com.org.oh_backend.Model.EmployeeMaster;
+
+public interface EmployeeMasterDAO {
+	
+	boolean saveEmployee(EmployeeMaster employee);
+	
+	EmployeeMaster searchEmployeeById(int employeeId);
+	
+	List<EmployeeMaster> getAllEmployees();	
+	
+	boolean deleteEmployee(int employeeId);
+	
+	boolean updateEmployee(EmployeeMaster employee);
+}
---
+++ oh-backend/src/main/java/com/org/oh_backend/DAO/EmployeeMasterDAOSpringJDBCTemplateImpl.java
@@ -0,0 +1,34 @@
+package com.org.oh_backend.DAO;
+
+import java.util.List;
+
+import com.org.oh_backend.Model.EmployeeMaster;
+
+public class EmployeeMasterDAOSpringJDBCTemplateImpl implements EmployeeMasterDAO {
+
+	public EmployeeMaster searchEmployeeById(int employeeId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<EmployeeMaster> getAllEmployees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean saveEmployee(EmployeeMaster employee) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean deleteEmployee(int employeeId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean updateEmployee(EmployeeMaster employee) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
